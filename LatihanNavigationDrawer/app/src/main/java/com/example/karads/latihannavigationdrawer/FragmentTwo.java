d3cfbe18d5fd35709465baeece048aea693ba3e5
DESKTOP-GM15L2O\karads <herry.sukwanto@putra-perdana.com>
2018-01-09 14:55:34 +0700

    Add simple properties file

--- LatihanNavigationDrawer/app/src/main/java/com/example/karads/latihannavigationdrawer/FragmentTwo.java
+++ LatihanNavigationDrawer/app/src/main/java/com/example/karads/latihannavigationdrawer/FragmentTwo.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.karads.latihannavigationdrawer.util.UtilProperties;
+
+import java.io.IOException;
 
 
 /**
@@ -23,7 +28,18 @@ public class FragmentTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_two, container, false);
+        View view =  inflater.inflate(R.layout.fragment_fragment_two, container, false);
+        TextView textView = view.findViewById(R.id.nama_from_config);
+
+        try {
+            String name = UtilProperties.getProperty("name", getActivity().getApplicationContext());
+            textView.setText(name);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return view;
     }
 
 }
---
+++ LatihanNavigationDrawer/app/src/main/java/com/example/karads/latihannavigationdrawer/util/UtilProperties.java
@@ -0,0 +1,22 @@
+package com.example.karads.latihannavigationdrawer.util;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by karads on 1/9/2018.
+ */
+
+public class UtilProperties {
+    public static String getProperty(String key,Context context) throws IOException {
+        Properties properties = new Properties();;
+        AssetManager assetManager = context.getAssets();
+        InputStream inputStream = assetManager.open("config.properties");
+        properties.load(inputStream);
+        return properties.getProperty(key);
+    }
+}
